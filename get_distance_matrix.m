2927a05f624dd3da8821b01dafd3a4217ec79200
edd26 <ed18abc@herts.ac.uk>
2019-10-09 14:18:40 +0100

    Added size restriction

--- get_distance_matrix.m
+++ get_distance_matrix.m
@@ -1,4 +1,5 @@
-function [dsit_matrix] = get_distance_matrix(dataset, distance_measure)
+function [dsit_matrix] = get_distance_matrix(dataset, distance_measure, ...
+                                                            restriction)
 % Compute distances between points stored in 'dataset'. Every column
 % represents cordinate and every row represents a data point.
 % 'distance_measure' determines type of distance measure used.
@@ -6,7 +7,10 @@ function [dsit_matrix] = get_distance_matrix(dataset, distance_measure)
 % Returns square matrix with interpoint distances.
 
 dataset_perm = dataset(randperm(size(dataset, 1)), :);
- 
+
+if restriction < size(dataset_perm, 1)
+    dataset_perm = dataset_perm(1:restriction, :);
+end
 distances = pdist(dataset_perm, distance_measure);
 dsit_matrix = squareform(distances);
 end
