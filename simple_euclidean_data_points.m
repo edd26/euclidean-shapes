a5e5f68f52b6f6a3f70dfeedddbcad0a151fb3ca
Emil Dmitruk <edmitruk@mion.elka.pw.edu.pl>
2019-11-27 14:18:29 +0000

    Added code for ordered matrices

--- simple_euclidean_data_points.m
+++ simple_euclidean_data_points.m
@@ -14,31 +14,66 @@ do_betti_analysis = true;
 alg_name = 'split';
 dens_range = 0.6;
 %% Load the matricies with the data
-loading = true;
-if loading
-    load("data/DIST_euc_geom_d2_n88.csv")
-    load("data/DIST_euc_geom_d5_n88.csv")
-    load("data/DIST_euc_geom_d15_n88.csv")
-    load("data/DIST_euc_rand_d2_n88.csv")
-    load("data/DIST_euc_rand_d5_n88.csv")
-    load("data/DIST_euc_rand_d15_n88.csv")
+loading_distances = false;
+loading_ordering = true;
+
+if loading_distances
+    load("data/DIST_euc_geom_d2_n69.csv")
+    load("data/DIST_euc_geom_d5_n69.csv")
+    load("data/DIST_euc_geom_d15_n69.csv")
+    load("data/DIST_euc_rand_d2_n69.csv")
+    load("data/DIST_euc_rand_d5_n69.csv")
+    load("data/DIST_euc_rand_d15_n69.csv")
+end
+
+if loading_ordering
+    load("data/ORD_euc_geom_d2_n69.csv")
+    load("data/ORD_euc_geom_d5_n69.csv")
+    load("data/ORD_euc_geom_d15_n69.csv")
+    load("data/ORD_euc_rand_d2_n69.csv")
+    load("data/ORD_euc_rand_d5_n69.csv")
+    load("data/ORD_euc_rand_d15_n69.csv")
 end
 % ===================================
 %% Compute and plot Betti curves
-size_restric = 88;
-if do_betti_analysis
-%     compute_and_plot_bettis(DIST_euc_geom_d2_n88, size_restric, alg_name, ...
-%                                             dens_range, 'geom_d2');
-%     compute_and_plot_bettis(DIST_euc_geom_d5_n88, size_restric, alg_name, ...
-%                                             dens_range, 'geom_d5');
-%     compute_and_plot_bettis(DIST_euc_geom_d15_n88, size_restric, alg_name, ...
-%                                             dens_range, 'geom_d15');
-    compute_and_plot_bettis(DIST_euc_rand_d2_n88, size_restric, alg_name, ...
-                                            dens_range, 'rand_d2');
-	compute_and_plot_bettis(DIST_euc_rand_d5_n88, size_restric, alg_name, ...
-                                            dens_range, 'rand_d5');
-    compute_and_plot_bettis(DIST_euc_rand_d15_n88, size_restric, alg_name, ...
-                                            dens_range, 'rand_d15');
+size_restric = 69;
+
+if do_betti_analysis && loading_distances
+    fprintf('Nont here\n')
+     compute_and_plot_bettis(DIST_euc_geom_d2_n69, size_restric, alg_name, ...
+                                             dens_range, 'geom_d2');
+     compute_and_plot_bettis(DIST_euc_geom_d5_n69, size_restric, alg_name, ...
+                                             dens_range, 'geom_d5');
+     compute_and_plot_bettis(DIST_euc_geom_d15_n69, size_restric, alg_name, ...
+                                             dens_range, 'geom_d15');
+%     compute_and_plot_bettis(DIST_euc_rand_d2_n69, size_restric, alg_name, ...
+%                                             dens_range, 'rand_d2');
+% 	compute_and_plot_bettis(DIST_euc_rand_d5_n69, size_restric, alg_name, ...
+%                                             dens_range, 'rand_d5');
+%     compute_and_plot_bettis(DIST_euc_rand_d15_n69, size_restric, alg_name, ...
+%                                             dens_range, 'rand_d15');
+end
+
+
+if do_betti_analysis && loading_ordering
+     compute_and_plot_bettis(ORD_euc_geom_d2_n69, size_restric, alg_name, ...
+                                             dens_range, 'geom_d2');
+    fprintf('\n\n =========== \n\n\n')
+     compute_and_plot_bettis(ORD_euc_geom_d5_n69, size_restric, alg_name, ...
+                                             dens_range, 'geom_d5');
+     fprintf('\n\n =========== \n\n\n')
+     compute_and_plot_bettis(ORD_euc_geom_d15_n69, size_restric, alg_name, ...
+                                             dens_range, 'geom_d15');
+    fprintf('\n\n =========== \n\n\n')
+     compute_and_plot_bettis(ORD_euc_rand_d2_n69, size_restric, alg_name, ...
+                                             dens_range, 'rand_d2');
+    fprintf('\n\n =========== \n\n\n')
+ 	 compute_and_plot_bettis(ORD_euc_rand_d5_n69, size_restric, alg_name, ...
+                                             dens_range, 'rand_d5');
+    fprintf('\n\n =========== \n\n\n')
+     compute_and_plot_bettis(ORD_euc_rand_d15_n69, size_restric, alg_name, ...
+                                             dens_range, 'rand_d15');
+    fprintf('\n\n =========== \n\n\n')
 end
 
 
